b911b1e219a2dd112d21ae691c4632fe216dff7d
Muqsit <sayedabdulmuqsit11@gmail.com>
2026-07-05 14:19:15 +0530

    Add Terraform VPC infrastructure project

---
+++ main.tf
@@ -0,0 +1,71 @@
+terraform {
+ required_providers{
+  aws={
+     source = "hashicorp/aws"
+     version="~> 5.0"
+      }
+   }
+}
+
+provider "aws" {
+region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  tags = {
+    Name = "terraform-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidr
+  availability_zone = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "terraform-public-subnet"
+    
+  
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidr
+  availability_zone = var.availability_zone
+  
+  tags = {
+    Name = "terraform-private-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "terraform-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "terraform-public-rt"
+    }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
